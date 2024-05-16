71c6b8ad5ade41ff9efd6959fa2b257294dce051
Masaki Hirabayashi <pektyer305@gmail.com>
2024-05-16 15:00:28 +0900

    meatadata into PostCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -20,31 +20,31 @@ export default function PostCard(props:any){
   }, [props.url]);
 
     return (
-        <div className="border rounded-lg shadow-sm overflow-hidden text-wrap">
+        <div className="border rounded-lg shadow-sm text-wrap whitespace-normal break-words">
           <div className="p-4 flex items-center">
             <img
               src={props.user.userImage}
               alt="profile"
               className="rounded-full h-8 w-8 object-cover"
             />
-            <span className="ml-2 font-semibold">{props.user.username}</span>
+            <div className="ml-2 font-semibold">{props.user.username}</div>
+            {props.url&&(<div className="flex-col flex">:{metadata?.title}</div>)}
           </div>
           <Splide className="flex justify-center">
               <SplideSlide>
                 <img src={props.Image} alt="Image 1" className="w-auto h-auto"/>
               </SplideSlide>
               {props.url&&(<SplideSlide className="flex justify-center items-center">
+              <a href={metadata?.url}>
               <img src={metadata?.image} alt="Image 2" className="w-auto h-auto"/>
+              </a>
               </SplideSlide>)}
             </Splide>
           <div className="px-4 pb-4">
             <span className="font-semibold mr-2">{props.title}</span><br/>
-            <span>{props.content}</span>
+            <span className="text-pretty">{props.content}</span>
             <br/>
             <span>{props.created}</span>
-          </div>
-          <div>
-            {(props.url)?"":""}
           </div>
         </div>
       );
